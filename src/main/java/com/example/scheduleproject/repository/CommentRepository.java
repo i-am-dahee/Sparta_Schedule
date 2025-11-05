6d6c5176bd35d28fca42c77c43f1d6d18c560af5
i-am-dahee <hxxly02@gmail.com>
2025-11-05 11:15:10 +0900

    [feat] 댓글 개수 제한 로직 #3

--- src/main/java/com/example/scheduleproject/repository/CommentRepository.java
+++ src/main/java/com/example/scheduleproject/repository/CommentRepository.java
@@ -4,4 +4,6 @@ import com.example.scheduleproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    int countByScheduleId(Long studentId);
 }
--- src/main/java/com/example/scheduleproject/service/CommentService.java
+++ src/main/java/com/example/scheduleproject/service/CommentService.java
@@ -17,8 +17,14 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final ScheduleRepository scheduleRepository;
 
+    // 댓글 생성
     @Transactional
     public CreateCommentResponse save(Long scheduleId, CreateCommentRequest request) {
+        int commentCount = commentRepository.countByScheduleId(scheduleId);
+        if (commentCount >= 10) {
+            throw new IllegalStateException("댓글은 최대 10개까지만 작성 가능합니다.");
+        }
+
         Schedule schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 일정입니다."));
 
